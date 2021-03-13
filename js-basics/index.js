53626117f6314823cf7a05ee091943b28e943f8b
czarrific <camua.117449@cubao.sti.edu.ph>
2021-03-13 10:37:57 +0800

    Initial commit

---
+++ js-basics/index.js
@@ -0,0 +1,55 @@
+ //This is my first JavaScript code!
+// console.log('Hello World');
+// let name = 'Czar';
+// console.log(name);
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number(1name)
+//Cannot contain a space  or hyphen(-)
+//Are case-sensitive
+
+// let firstName = 'Czarina';
+// let   lastName ='Camua';
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// let nickname = 'Czar'; //String Literal
+// let age = 30; //number literal
+// let isApproved = true; //boolean literal
+// let firstName= undefined;
+// let selectedColor = null;
+
+// let person = {
+//     nickname: 'Czar',
+//     age: 30
+// };
+
+// //Dot Notation
+// person.nickname = 'Inah';
+
+// //Bracket Notation 
+// let selection = 'nickname';
+// person[selection] = 'Yna';
+
+// console.log(person.nickname);
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+//Performing a task
+// function greet(nickname, lastName){
+//     console.log('Hello ' + nickname + ' ' + lastName);
+// }
+// greet('Czar', 'Camua');
+// greet('Pj', 'Arago');
+
+//Calculating a value
+function square(number){
+  return  number * number;
+}
+
+// let number = square(2);
+console.log(square(2));
+
+
